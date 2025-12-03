e083d8bf0d7816db29f5da1d4505b7ceb450570f
Lancellot <assis.neto@rede.ulbra.br>
2025-12-02 21:35:58 -0300

    chore: update  typescript dependencies

--- src/app.module.ts
+++ src/app.module.ts
@@ -6,14 +6,14 @@ import { PostagemModule } from './postagem/postagem.module';
 @Module({
   imports: [
     TypeOrmModule.forRoot({
-      type: 'mysql',
+      type: process.env.DB_TYPE as 'mysql',
       host: process.env.DB_HOST,
       port: Number(process.env.DB_PORT),
       username: process.env.DB_USERNAME,
       password: process.env.DB_PASSWORD,
       database: process.env.DB_DATABASE,
       entities: [Postagem],
-      synchronize: true,
+      synchronize: process.env.DB_SYNCHRONIZE === 'development',
     }),
     PostagemModule
   ],
