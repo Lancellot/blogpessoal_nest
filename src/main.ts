8417adb9d9b79e31936cdcdccc8140806ae91f12
Lancellot <assis.neto@rede.ulbra.br>
2025-12-16 15:07:15 -0300

    chore: alinhar imports de tema.entity e preparar e2e com sqlite
    - corrigir imports de tema.entities -> tema.entity
    - adicionar sqlite3 para testes e2e
    - ajustar jwt secret default
    - mover e2e para usuario.e2e-spec e remover app.e2e-spec

--- src/main.ts
+++ src/main.ts
@@ -1,9 +1,20 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { ValidationPipe } from '@nestjs/common';
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+
+  const config = new DocumentBuilder()
+    .setTitle('Blog Pessoal')
+    .setDescription('API do Blog Pessoal')
+    .setVersion('1.0')
+    .setContact('Generation Brasil', 'https://brazil.generation.org', 'generation@example.com')
+    .addBearerAuth()
+    .build();
+    const document = SwaggerModule.createDocument(app, config);
+    SwaggerModule.setup('/swagger', app, document);
 
   process.env.TZ = '-03:00';
 
--- src/usuario/entities/usuario.entity.ts
+++ src/usuario/entities/usuario.entity.ts
@@ -2,6 +2,7 @@ import { IsEmail, IsNotEmpty, MinLength } from "class-validator"
 import { Column, Entity, OneToMany, PrimaryGeneratedColumn } from "typeorm"
 import { Postagem } from "../../postagem/entities/postagem.entity"
 import { Exclude } from "class-transformer"
+import { ApiProperty } from "@nestjs/swagger"
 
 @Entity({name: "tb_usuarios"})
 export class Usuario {
@@ -9,24 +10,29 @@ export class Usuario {
     @PrimaryGeneratedColumn() 
     id: number
 
+    @ApiProperty()
     @IsNotEmpty()
     @Column({length: 255, nullable: false}) 
     nome: string
 
+    @ApiProperty({ description: 'Email do usuário', example: 'usuario@example.com' })
     @IsEmail()
     @IsNotEmpty()
     @Column({length: 255, nullable: false })
     email: string
 
+    @ApiProperty({ description: 'Senha do usuário', minLength: 8 })
     @Exclude({ toPlainOnly: true })
     @MinLength(8)
     @IsNotEmpty()
     @Column({length: 255, nullable: false }) 
     senha: string
 
+    @ApiProperty()
     @Column({length: 5000 }) 
     foto: string
 
+    @ApiProperty()
     @OneToMany(() => Postagem, (postagem) => postagem.usuario)
     postagem: Postagem[]
 
